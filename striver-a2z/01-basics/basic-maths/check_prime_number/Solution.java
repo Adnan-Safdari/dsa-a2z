79861962b9e32755f615f600d803d37705be7c18
Adnan-Safdari <safdariadnan@gmail.com>
2026-08-04 22:27:15 +0530

    Refactor prime number check

--- striver-a2z/01-basics/basic-maths/check_prime_number/Solution.java
+++ striver-a2z/01-basics/basic-maths/check_prime_number/Solution.java
@@ -2,36 +2,27 @@ import java.util.ArrayList;
 
 public class Solution {
 
-    public static boolean check_prime(int n){
-        ArrayList<Integer> divisors = listDivisors(n);
+    public static boolean checkPrime(int n) {
 
-        if (divisors.size() <= 2){
-            return true;
+        // 0 and 1 are not prime
+        if (n <= 1) {
+            return false;
         }
-        return false;
+
+        int limit = (int) Math.sqrt(n);
+
+        for (int i = 2; i <= limit; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     public static void main(String[] args) {
-        String result = (check_prime(7) ? "TRUE: It is a prime number" : "FALSE: It is a composite number");
+        String result = (checkPrime(7) ? "TRUE: It is a prime number" : "FALSE: It is a composite number");
         System.out.println(result);
     }
 
-    public static ArrayList<Integer> listDivisors(int n){
-
-        ArrayList<Integer> divisors = new ArrayList<>();
-        double limit = Math.sqrt(n); 
-        
-        for(int i=1; i<=limit; i++){
-            if (n % i == 0){
-                divisors.add(i);
-
-                int partner = n / i;
-                if (partner != i){
-                    divisors.add(partner);
-                }
-            }
-        }
-        divisors.sort(null);
-        return divisors;
-    }
 }
